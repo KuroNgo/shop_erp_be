da6e1828264a7e58a97586ab66b406880c72c0b9
KuroNgo <hoaiphong01012002@gmail.com>
2024-10-01 11:39:39 +0700

    upd: test db

--- infrastructor/db_t.go
+++ infrastructor/db_t.go
@@ -8,7 +8,7 @@ import (
 )
 
 func SetupTestDatabase(t *testing.T) (*mongo.Client, *mongo.Database) {
-	clientOpts := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOpts := options.Client().ApplyURI("mongodb+srv://andrew2611:11062001Phong@andrew.8ulkv.mongodb.net/?retryWrites=true&w=majority&appName=Andrew")
 	client, err := mongo.Connect(context.Background(), clientOpts)
 	if err != nil {
 		t.Fatalf("Failed to connect to MongoDB: %v", err)
